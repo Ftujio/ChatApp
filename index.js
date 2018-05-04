9f883f16907a3d8f481c3f3f2bc86733c28c5d95
Ftujio <svetlin954@abv.bg>
2018-05-05 01:36:51 +0300

    Serving assets folder and improved some stuff.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
+app.use(express.static('assets'));
+
+app.listen(3000, () => {
   console.log('listening on *:3000');
 });
